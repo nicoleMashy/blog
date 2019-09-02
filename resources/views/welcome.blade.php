4550f3695e99d6a77011d737f77a28816588814e
nicoleMashy <nicolemashaishe@gmail.com>
2019-09-02 18:24:33 +0200

    home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
+
         <link rel="stylesheet" href="{{ asset('/css/custom.css') }}" type="text/css">
     </head>
 <body>
@@ -18,12 +19,12 @@
             <!-- Example responsive navigation menu  -->
             <a class="show" href="#menu">Menu</a><a class="hide" href="#hidemenu">Menu</a>
             <ul class="menu-items">
-                <li><a style="" href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Contact</a></li>
-                <li><a href="">Twitter</a></li>
-                <li><a href="">Facebook</a></li>
-                <li><a href="">Twitter</a></li>
+                <li><a style="" href=""><img src="/images/.png" class="card-img-top" alt="..."></a></li>
+                <li><a href="">home</a></li>
+                <li><a href="">products</a></li>
+                <li><a href="">gallery</a></li>
+                <li><a href="">product tips</a></li>
+                <li><a href="">sign in</a></li>
             </ul>
         </div>
     </nav>
@@ -34,20 +35,12 @@
             <p class="lead">SHOP WITH US <span>FOR ALL detergents </span></p>
         </div>
     </div>
-    <div class="container">
+<div class="container">
     <div class="rows">
         <div class="column" style="background-color: white;">
-            <div class="tab">
-                <button class="tablinks" style="background-color:brown ;" >Browse by category</button>
-                <button class="tablinks" >detergents</button>
-                <button class="tablinks">laundry</button>
-                <button class="tablinks">household products</button>
-                <button class="tablinks">view all</button>
-            </div>
-        </div>
-    </div>
 
-        <div class="rows">
+        </div>
+
             <div class="column" style="background-color:white;">
 
                 <div class="row">
@@ -61,21 +54,36 @@
                             </div>
 
                         </div>
+
+
+
                     </div>
 
                     <div class="columnd">
-                        <div class="card">
-                            <h3>Card 2</h3>
-                            <p>Some text</p>
-                            <p>Some text</p>
+
+                        <div class="card" style="width: 18rem; height: 400px;">
+                            <img src="/images/product.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text" style="text-color: maroon">Sk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+
                         </div>
-                    </div>
+
+                        </div>
+
 
                     <div class="columnd">
-                        <div class="card">
-                            <h3>Card 3</h3>
-                            <p>Some text</p>
-                            <p>Some text</p>
+                        <div class="card" style="width: 18rem; height: 400px;">
+                            <img src="/images/product.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text" style="text-color: maroon">Sk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+
+                        </div>
                         </div>
                     </div>
 
@@ -86,5 +94,49 @@
 
     </div>
     </div>
+<div class="container">
+    <div class="topnav">
+
+        <!-- Centered link -->
+        <div class="topnav-centered">
+            <a href="#home" class="active">Home</a>
+        </div>
+
+        <!-- Left-aligned links (default) -->
+        <a href="#news"> <img src="/images/book.png" class="card-img-top" alt="..."></a>
+        <a href="#contact"> <img src="/images/twitter.png" class="card-img-top" alt="..."></a>
+        <a href="#contact"> <img src="/images/youtube.png" class="card-img-top" alt="..."></a>
+        <a href="#contact"> <img src="/images/instagram.png" class="card-img-top" alt="..."></a>
+        <a href="#contact"> <img src="/images/pintres.png" class="card-img-top" alt="..."></a>
+
+        <!-- Right-aligned links -->
+        <div class="topnav-right">
+            <a ><input type="text" placeholder="Search.."></a>
+            <a href="#about">sign up</a>
+        </div>
+
+    </div>
+    <div class="rows">
+        <div class="columnd" style="background-color:white; text-align: center;">
+            <h2>help and support</h2>
+            <p>Some text..</p>
+        </div>
+        <div class="columnd" style="background-color:white; text-align: center;">
+            <h2>shopping with us</h2>
+            <p>Some text..</p>
+        </div>
+        <div class="columnd" style="background-color:white; text-align: center;">
+            <h2>view cart</h2>
+            <p>Some text..</p>
+        </div>
+    </div>
+
+
+</div>
+    <footer id="footer" class="modern-footer"><div class="container">
+            <nav id="footer_nav"><ul class="inline"><li class="footer-item-616 "> <a data-ct-action="Navigation Click" data-ct-category="Custom" data-ct-identifier="Footer_nav-616" data-ct-information="Footer Navigation-Terms of use" title="Terms of use" href="http://www.unilever.co.za/resource/legalnotice/" class="navItemLink">Terms of use</a></li><li class="footer-item-32 "> <a data-ct-action="Navigation Click" data-ct-category="Custom" data-ct-identifier="Footer_nav-32" data-ct-information="Footer Navigation-Privacy Notice" aria-label='' target="_blank" title="Privacy Notice" href="http://www.unilevernotices.com/south-africa/english/privacy-notice/notice.html" class="navItemLink">Privacy Notice</a></li><li class="footer-item-29 "> <a data-ct-action="Navigation Click" data-ct-category="Custom" data-ct-identifier="Footer_nav-29" data-ct-information="Footer Navigation-Cookie Notice" aria-label='' target="_blank" title="Cookie Notice" href="" class="navItemLink">Cookie Notice</a></li><li class="footer-item-617 "> <a data-ct-action="Navigation Click" data-ct-category="Custom" data-ct-identifier="Footer_nav-617" data-ct-information="Footer Navigation-Accessibility" title="Accessibility" href="http://www.unilever.co.za/resource/accessibility.aspx" class="navItemLink">Accessibility</a></li><li class="footer-item-1598 "> <a data-ct-action="Navigation Click" data-ct-category="Custom" data-ct-identifier="Footer_nav-1697" data-ct-information="Footer Navigation-About" title="About" href="https://www.sunlight.co.za/about-us" class="navItemLink">About</a></li><li class="footer-item-6 "> <a data-ct-action="Navigation Click" data-ct-category="Custom" data-ct-identifier="Footer_nav-11" data-ct-information="Footer Navigation-Contact" title="Contact" href="" class="navItemLink">Contact</a></li><li class="footer-item-927 "> <a data-ct-action="Navigation Click" data-ct-category="Custom" data-ct-identifier="Footer_nav-927" data-ct-information="Footer Navigation-Unilever" aria-label='' target="_blank" title="Unilever" href="" class="navItemLink">Sterling Haven</a></li><li class="footer-item-954 "> <a data-ct-action="Navigation Click" data-ct-category="Custom" data-ct-identifier="Footer_nav-956" data-ct-information="Footer Navigation-T's &amp; C's" title="T's &amp; C's" href="" class="navItemLink">T's &amp; C's</a></li><li class="footer-item-8 "> <a data-ct-action="Navigation Click" data-ct-category="Custom" data-ct-identifier="Footer_nav-1664" data-ct-information="Footer Navigation-Site Map" title="Site Map" href="" class="navItemLink">Site Map</a></li></ul>
+            </nav> <aside class="footer-banner clearfix"><p>© Copyright 2019 star mashaishe All rights reserved</p><p class="site-footer"></p> </aside></div>
+    </footer>
+
     </body>
 </html>
